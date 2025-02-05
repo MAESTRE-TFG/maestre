a2da1cd2c5cd623e6eb4ed3cf2a6a29586b9cfb2
Rafael Pulido Cifuentes <rafpulcif@alum.us.es>
2025-02-05 10:55:01 +0100

    Actualización de dependencias y creación del requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Add this
     'api',  # Add this
+    'corsheaders',  # Add this
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Add this at the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
