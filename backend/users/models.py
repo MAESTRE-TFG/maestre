6a3d50d86e954bd4eae78719ad3394659ff9687e
Rafa <rafpulcif@alum.us.es>
2025-02-12 23:48:01 +0100

    feat: añadir token a los usuarios para confirmar autenticación

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
-class CustomUser(AbstractUser):
+'''
+Como tenemos un modelo personalizado de usuario, necesitamos un administrador de usuario personalizado
+que herede de BaseUserManager. Este administrador de usuario personalizado se encargará de crear
+usuarios y superusuarios. Esto nos libra de mucha faena, se puede comprobar que todo está mucho mas simple 
+en http://localhost:8000/admin/
+'''
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     region = models.CharField(max_length=100, null=True, blank=True, default=None)
@@ -28,4 +62,4 @@ class CustomUser(AbstractUser):
         ordering = ['username']
 
     def __str__(self):
-        return f"{self.name} {self.surname} ({self.username})"
+        return self.email
