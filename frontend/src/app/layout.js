a03d5447fe6b44e6a5370505c1c02def4d8ac26c
Rafael Pulido Cifuentes <rafpulcif@alum.us.es>
2025-05-04 18:29:04 +0200

    feat(layout): add RootLayout component with theme and font support
    The RootLayout component integrates theme and font configurations using ThemeProvider and Google Fonts. This ensures consistent styling and theming across the application.

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,38 @@
+"use client";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Alfa_Slab_One } from 'next/font/google';
+import { ThemeProvider } from "@/components/theme-provider";
+import React from 'react';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const alfaSlabOne = Alfa_Slab_One({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} ${alfaSlabOne.className}`}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
