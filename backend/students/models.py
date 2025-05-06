1457ea4628277ce756bde3ae6fea9838322e22ad
Antonio Macías <antmacfer1@alum.us.es>
2025-05-06 12:28:45 +0200

    test: coverage improvement on classroom tests

--- backend/students/models.py
+++ backend/students/models.py
@@ -5,7 +5,7 @@ from classrooms.models import Classroom
 class Student(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='students')
 
     def __str__(self):
         return self.name
